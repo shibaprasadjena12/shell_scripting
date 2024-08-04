931ff7563313c4e1b0a101d713d7248140913f2c
shibaprasad jena <102553029+shibaprasadjena12@users.noreply.github.com>
2024-08-04 21:36:56 +0000

    1st commit

---
+++ my-test.sh
@@ -0,0 +1,1 @@
+for i in {1..100}; do echo $i; done #one line code that prints 1 to 100
---
+++ test.sh
@@ -0,0 +1,15 @@
+name=ritu
+
+
+echo "my name is : " $name
+
+new_name=$name
+
+echo $new_name
+
+if [$name=='ritu'];
+then
+    echo "if"
+else
+    echo "else"
+fi
